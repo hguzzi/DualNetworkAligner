bd1e89be6d8e3b76ab96aa804cdf25abc2d2a052
Emanuel Salerno <emans.89@hotmail.it>
2020-09-27 10:09:19 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -13,22 +13,25 @@ from datetime import datetime
 
 
 # EXAMPLE
-t1 = datetime.now()
+
 # Folder with the name of dataset to analize
 datasetNameFolder = "STRING/"  # <-- MODIFY to select dataset
 # Directory path of dataset to analize
-inputDirectotyPath = "Dataset/"+datasetNameFolder + "Input/"  
+inputDirectotyPath = "Dataset/"+datasetNameFolder + "Input/"
 # Directory path to store results
-outputDirectoryPath = "Dataset/"+datasetNameFolder + "Output/"  
+outputDirectoryPath = "Dataset/"+datasetNameFolder + "Output/"
+
+t1 = datetime.now()  # used to calculate the execution time
+print(t1, " --> STARTED...")
+print("Dataset/"+datasetNameFolder)
 
 # INPUT file
 # Text file of weighted graph
-weightedGraphFile = inputDirectotyPath+"weightedGraph.txt" 
-print(weightedGraphFile)
+weightedGraphFile = inputDirectotyPath+"weightedGraph.txt"
 # Text file of weighted graph
-unweightedGraphFile = inputDirectotyPath+"unweightedGraph.txt" 
+unweightedGraphFile = inputDirectotyPath+"unweightedGraph.txt"
 # Text file of Similarity relationship among dual networks nodes
-simFile = inputDirectotyPath+"similarityFile.txt" 
+simFile = inputDirectotyPath+"similarityFile.txt"
 
 # OUTPUT file
 # Compressed Pickle file of the weighted graph
@@ -40,36 +43,45 @@ alignmentGraphPickle = outputDirectoryPath+"alignmentGraph.gz2"
 # Compressed Pickle file of the DCS graph
 dcsGraphPickle = outputDirectoryPath+"dcs.gz2"
 
-print(datetime.now(), " --> build weighted graph W...")
+# STEP ONE: build the graphs
+
 # Builds the weighted graph W
+print(datetime.now(), " --> build weighted graph W...")
 W = buildGraph(weightedGraphFile, skipLines=0,
                splitSep=" ", weightedEdges=True)
-# Stores the weighted graph in a pickle file
-nx.write_gpickle(W, weightedGraphPickle)
 print(datetime.now(), " --> W: nodes ",
       len(W.nodes()), " edges: ", len(W.edges()))
 
-print(datetime.now(), " --> build unweighted graph U...")
+# Stores the weighted graph in a pickle file
+# nx.write_gpickle(W, weightedGraphPickle) # UNCOMMENT this line if you want to save a graph
+
 # Builds the unweighted graph U
+print(datetime.now(), " --> build unweighted graph U...")
 U = buildGraph(unweightedGraphFile, skipLines=0,
                splitSep=" ", weightedEdges=False)
-# Stores the unweighted graph in a pickle file
-nx.write_gpickle(U, unweightedGraphPickle)
 print(datetime.now(), " --> U: nodes ",
       len(U.nodes()), " edges: ", len(U.edges()))
 
-print(datetime.now(), " --> build alignment graph A...")
+# Stores the unweighted graph in a pickle file
+# nx.write_gpickle(U, unweightedGraphPickle) # UNCOMMENT this line if you want to save a graph
+
+# STEP TWO
 # Builds the alignment graph A
+print(datetime.now(), " --> build alignment graph A...")
 A = pairwiseAlignment(U, W, k=5, simTxt=simFile, skipLines=0, splitSep="-")
-# Stores the alignment graph in a pickle file
-nx.write_gpickle(A, alignmentGraphPickle)
 print(datetime.now(), " --> alignGraph A: nodes ",
       len(A.nodes()), " edges: ", len(A.edges()))
 
-print(datetime.now(), " --> Search DCS in alignment graph...")
+# Stores the alignment graph in a pickle file
+nx.write_gpickle(A, alignmentGraphPickle) # COMMENT this line if you don't want to save a graph
+
+# LAST STEP
 # Extracts the DCS from the alignment graph
+print(datetime.now(), " --> Search DCS in alignment graph...")
 dcsA = extractDCS(A)
 # Stores the DCS graph of A in a pickle file
 nx.write_gpickle(dcsA, dcsGraphPickle)
 
-print("Execution time: ", datetime.now()-t1)
+t2 = datetime.now()
+print(t2, " --> FINISHED")
+print("Execution time: ", t2-t1)
